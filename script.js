2efeff24389d6c053de413f2b106f8bb44c9e63f
hemant1883 <hemantrana1883@gmail.com>
2024-12-16 11:26:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let yes=0;
+let no=0;
+
+function increaseYes(){
+yes++;
+document.getElementById("yes").innerHTML="Yes : " + yes
+}
+function increaseNo(){
+    no++;
+    document.getElementById("no").innerHTML="No : " + no
+}
